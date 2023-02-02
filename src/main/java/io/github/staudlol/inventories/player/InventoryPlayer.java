7071ccc2e7227bb66a89c8e66e999ee7ca6460af
Staud <77198963+staudlol@users.noreply.github.com>
2023-02-02 16:31:23 +0000

    added base for the inventory of the player

---
+++ src/main/java/io/github/staudlol/inventories/player/InventoryPlayer.java
@@ -0,0 +1,32 @@
+package io.github.staudlol.inventories.player;
+
+import lombok.Data;
+import org.bukkit.configuration.serialization.ConfigurationSerializable;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Data
+public class InventoryPlayer implements ConfigurationSerializable {
+
+    public final UUID uniqueId;
+
+    public ItemStack[] contents = new ItemStack[36];
+    public ItemStack[] armorContents = new ItemStack[4];
+
+    /**
+     * Constructor to create a new {@link InventoryPlayer
+     *
+     * @param uniqueId the unique identifier.
+     */
+
+    public InventoryPlayer(UUID uniqueId) {
+        this.uniqueId = uniqueId;
+    }
+
+    @Override
+    public Map<String, Object> serialize() {
+        return null;
+    }
+}
