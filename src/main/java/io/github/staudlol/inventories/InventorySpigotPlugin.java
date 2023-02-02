59f605430f66b2c8d0b3b6fe27649512bf65d70e
Staud <77198963+staudlol@users.noreply.github.com>
2023-02-02 16:28:05 +0000

    first commit

---
+++ src/main/java/io/github/staudlol/inventories/InventorySpigotPlugin.java
@@ -0,0 +1,16 @@
+package io.github.staudlol.inventories;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class InventorySpigotPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
